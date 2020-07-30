1f3a36cd0a54417a51116dc52bd859c64cfc3718
Jörg Encke <joerg.encke@uni-oldenburg.de>
2020-07-30 12:39:06 +0200

    added python version requirement (>3) in setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     name = "audiotools",
     version = "0.01",
     packages = find_packages(),
-
     author = "Joerg Encke",
     author_email = "joerg.encke@tum.de",
     description = "Toolbox of some simple functions for audio signal generation",
+    python_requires='>=3',
     license = "GPL",
 )
