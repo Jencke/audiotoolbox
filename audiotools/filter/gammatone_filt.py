3d31b4c7c5c7413ea90553c985275e0c01f38399
Jörg Encke <joerg.encke@uni-oldenburg.de>
2021-06-21 20:27:07 +0200

    moved gammatone filter into the gammatone file

--- audiotools/filter/gammatone_filt.py
+++ audiotools/filter/gammatone_filt.py
@@ -100,3 +100,57 @@ def gammatonefos_apply(signal, b, a, order, states=None):
         b = np.ones_like(b)
 
     return signal, states
+
+
+def gammatone(signal, fc, bw, fs, order=4, attenuation_db='erb', return_complex=True):
+    """Apply a gammatone filter to the signal
+
+    Applys a gammatone filter following [1]_ to the input signal
+    and returns the filtered signal.
+
+    Parameters
+    ----------
+    signal : ndarray
+        The input signal
+    fs : int
+      The sample frequency in Hz
+    fc : scalar
+      The center frequency of the filter in Hz
+    bw : scalar
+      The bandwidth of the filter in Hz
+    order : int
+      The filter order (default = 4)
+    attenuation_db: scalar or 'erb'
+      The attenuation at half bandwidth in dB (default = -3).
+      If set to 'erb', the bandwidth is interpreted as the rectangular
+      equivalent bw
+    return_complex : bool
+      Whether the complex filter output or only it's real
+      part is returned (default = True)
+
+    Returns
+    -------
+      The filtered signal.
+
+    References
+    ----------
+    .. [1] Hohmann, V., Frequency analysis and synthesis using a
+          Gammatone filterbank, Acta Acustica, Vol 88 (2002), 43 -3442
+
+    """
+
+    b, a = design_gammatone(fc, bw, fs, order, attenuation_db)
+
+    out_signal = np.zeros_like(signal, complex)
+
+    if signal.ndim > 1:
+        n_channel = signal.shape[1]
+        for i_c in range(n_channel):
+            out_signal[:, i_c], _ = gammatonefos_apply(signal[:, i_c], b, a, order)
+    else:
+        out_signal[:], _ = gammatonefos_apply(signal, b, a, order)
+
+    if not return_complex:
+        out_signal = out_signal.real
+
+    return out_signal
