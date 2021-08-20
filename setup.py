0f1a1af5ef667e7d360fc6e6818c49d3649bc381
Jörg Encke <joerg.encke@posteo.de>
2021-08-20 09:06:27 +0200

    updated setup.py and pyproject.toml

--- setup.py
+++ setup.py
@@ -7,15 +7,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="audiotools",
-    version="0.01",
-    packages=find_packages(),
-    author="Joerg Encke",
-    author_email="joerg.encke@uol.de",
-    description="Toolbox for generating signals used in auditory research",
+    version="0.02",
+    author="Jörg Encke",
+    author_email="joerg.encke@posteo.de",
+    description="Toolbox for generating and working with audio signals",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://jencke.github.io/audiotools/",
     python_requires='>=3',
+    # packages=find_packages(),
+    package_dir={"" : "audiotools"},
+    packages=find_packages(where="audiotools"),
     install_requires=[
         'numpy>=1.19',
         'scipy>=1.5.1',
@@ -23,5 +25,10 @@ setup(
         'pytest>=5.4.3',
         'coverage'
     ],
-    license="GPL",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Scientific/Engineering"
+    ],
 )
