1c8cd95541bc89b9165e7df8e1251516cb4de50d
Jörg Encke <joerg.encke@posteo.de>
2023-12-15 11:13:56 +1100

    formatting and moving from np.math to math

--- audiotools/filter/gammatone_filt.py
+++ audiotools/filter/gammatone_filt.py
@@ -2,9 +2,10 @@ import numpy as np
 from .. import audiotools as audio
 from numpy import pi
 from scipy.signal import lfilter
+from math import factorial
 
 
-def design_gammatone(fc, bw, fs, order=4, attenuation_db='erb'):
+def design_gammatone(fc, bw, fs, order=4, attenuation_db="erb"):
     """Return the coefficient of a gammatone filter.
 
     Calculates the filter coefficents for a gammatone filter following
@@ -42,37 +43,34 @@ def design_gammatone(fc, bw, fs, order=4, attenuation_db='erb'):
     """
     # in case the bandwith is stated in equivalent rectangular
     # bandwidth:
-    if attenuation_db == 'erb':
+    if attenuation_db == "erb":
         # Using Eq. 14 and 15 [Hohmann2002]
         bw = erb_to_3db(bw, order)
         attenuation_db = -3
 
-    phi = pi * bw / fs          # Eq. 12 [Hohmann2002]
-    beta = 2 * pi * fc / fs     # Eq. 10 [Hohmann2002]
+    phi = pi * bw / fs  # Eq. 12 [Hohmann2002]
+    beta = 2 * pi * fc / fs  # Eq. 10 [Hohmann2002]
 
-    alpha = 10**(0.1 * attenuation_db / order)       # Eq. 12 [Hohmann2002]
+    alpha = 10 ** (0.1 * attenuation_db / order)  # Eq. 12 [Hohmann2002]
     p = (-2 + 2 * alpha * np.cos(phi)) / (1 - alpha)  # Eq. 12 [Hohmann2002]
 
     lvar = -p / 2 - np.sqrt(p**2 / 4 - 1)  # Eq. 12 [Hohmann2002]
 
-    coef = lvar * np.exp(1j * beta)   # Eq. 1 [Hohmann2002]
-    factor = 2 * (1 - np.abs(coef))**order
+    coef = lvar * np.exp(1j * beta)  # Eq. 1 [Hohmann2002]
+    factor = 2 * (1 - np.abs(coef)) ** order
 
-    b = np.array(factor),
-    a = np.array([1., -coef])
+    b = (np.array(factor),)
+    a = np.array([1.0, -coef])
 
     return b, a
 
 
-def erb_to_3db(
-        equivalent_rect_bw: float,
-        order: int
-) -> float:
+def erb_to_3db(equivalent_rect_bw: float, order: int) -> float:
     """ERB to -3db bw conversion for gammatone filter of given order."""
-    c = 2 * np.sqrt(2**(1 / order) - 1)
-    alpha = ((np.pi * np.math.factorial(2 * order - 2)
-              * 2.**(2 - 2 * order))
-             / np.math.factorial(order - 1)**2)
+    c = 2 * np.sqrt(2 ** (1 / order) - 1)
+    alpha = (np.pi * factorial(2 * order - 2) * 2.0 ** (2 - 2 * order)) / factorial(
+        order - 1
+    ) ** 2
     bw = c / alpha * equivalent_rect_bw
     return bw
 
@@ -110,17 +108,16 @@ def gammatonefos_apply(signal, b, a, order, states=None):
         if np.ndim(n_channel) == 0:
             if n_channel == 1:  # only one channel
                 shape = [order, 1]
-            else:               # more then one channels
+            else:  # more then one channels
                 shape = [order, 1, n_channel]
-        else:                   # Multiple dimensions
+        else:  # Multiple dimensions
             shape = [order, 1, *n_channel]
 
     states = np.zeros(shape, dtype=np.complex128)
 
     # copy results into a new complex Signal or array
     if isinstance(signal, audio.Signal):
-        signal_out = audio.Signal(n_channel, signal.duration,
-                                  signal.fs, dtype=complex)
+        signal_out = audio.Signal(n_channel, signal.duration, signal.fs, dtype=complex)
     else:
         signal_out = np.zeros_like(signal, dtype=complex)
     signal_out[:] = signal
@@ -135,8 +132,7 @@ def gammatonefos_apply(signal, b, a, order, states=None):
     return signal_out, states
 
 
-def gammatone(signal, fc, bw, fs, order=4, attenuation_db='erb',
-              return_complex=True):
+def gammatone(signal, fc, bw, fs, order=4, attenuation_db="erb", return_complex=True):
     """Apply a gammatone filter to the signal.
 
     Applys a gammatone filter following [Hohmann2002]_ to the input
