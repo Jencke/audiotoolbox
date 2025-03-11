436438b08ef133ce98a07df125ac3d4fd35b609d
Jörg Encke <joerg.encke@posteo.de>
2025-03-11 16:28:55 +1100

    add docs workflow

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,7 +14,10 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
+import os
+import sys
 
+sys.path.insert(0, os.path.abspath(os.path.join("../..", "src")))
 # -- Project information -----------------------------------------------------
 
 project = "audiotoolbox"
