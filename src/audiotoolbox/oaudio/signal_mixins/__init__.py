ac05a763e29cb286740608ba9e62f6370b531ca7
Jörg Encke <joerg.encke@posteo.de>
2025-08-08 08:53:29 +0200

    added generation mixin

--- src/audiotoolbox/oaudio/signal_mixins/__init__.py
+++ src/audiotoolbox/oaudio/signal_mixins/__init__.py
@@ -1,15 +1,17 @@
 """Signal mixins for organizing Signal class functionality."""
 
-# from .generation import GenerationMixin
-# from .modification import ModificationMixin
+from .generation import GenerationMixin
 from .analysis import AnalysisMixin
+
+# from .modification import ModificationMixin
+
 
 # from .io import IOMixin
 # from .filtering import FilteringMixin
 # from .utilities import UtilitiesMixin
 
 __all__ = [
-    # "GenerationMixin",
+    "GenerationMixin",
     # "ModificationMixin",
     "AnalysisMixin",
     # "IOMixin",
