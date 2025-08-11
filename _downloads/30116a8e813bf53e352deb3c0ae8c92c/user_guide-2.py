0172028a9ab1f8c7b5f9971aa9362d22ba6dd359
Jencke <Jencke@users.noreply.github.com>
2025-08-11 11:14:12 +0000

    deploy: b615bf453b3ec5b8881143c01b5830e417307d07

--- _downloads/30116a8e813bf53e352deb3c0ae8c92c/user_guide-2.py
+++ _downloads/30116a8e813bf53e352deb3c0ae8c92c/user_guide-2.py
@@ -1,13 +1,9 @@
-import audiotoolbox as audio
-import matplotlib.pyplot as plt
-
-sig = audio.Signal(n_channels=1, duration=100e-3, fs=48000)
+sig = audio.Signal(1, 100e-3, 48000)
 sig.add_tone(frequency=500, amplitude=1, start_phase=0)
 sig.add_fade_window(rise_time=30e-3, type='cos')
 
 plt.plot(sig.time * 1e3, sig)
 plt.xlabel('Time / ms')
 plt.ylabel('Amplitude')
-plt.title('Tone with Raised Cosine Fade-in and -out')
-plt.grid(True)
+plt.title('Tone with raised cos fade-in and -out')
 plt.show()
--- _downloads/fcec1221ec1318266f390925d76a41ea/user_guide-7.py
+++ _downloads/fcec1221ec1318266f390925d76a41ea/user_guide-7.py
@@ -3,17 +3,19 @@ sig.add_tone(500).set_dbfs(0)
 sig.add_noise("pink")
 sig.add_fade_window(10e-3)
 
+bank = audio.filter.bank.create_filterbank(fc=[250, 500, 1000], bw=[25, 50, 100], filter_type="butter", fs=sig.fs)
 
-spec, fc = sig.time_frequency.gammatone_specgram(
-    nperseg=1024, noverlap=512, flow=16, fhigh=16000, step=1 / 3
+
+spec, fc = sig.time_frequency.filterbank_specgram(bank=bank,
+    nperseg=1024, noverlap=512
 )
 fig, ax = plt.subplots(1, 1)
 cb = ax.pcolormesh(spec.time, fc, spec.T)
 ax.set_yscale("log")
-ax.set_ylim(16, 16000)
+ax.set_ylim(100, 1500)
 ax.set_ylabel("Frequency / Hz")
 ax.set_xlabel("Time / s")
-ax.set_title("Gammatone spectrogram")
+ax.set_title("Custom filter-bank spectrogram")
 cb = plt.colorbar(cb, ax=ax)
 cb.set_label("dB FS")
 plt.show()
