1916fc89bf0f5882f3cef8093e0a5f2d571fe86d
Jörg Encke <joerg.encke@posteo.de>
2026-03-23 15:32:07 +1100

    Add spectrum plotting method to Visualization class

--- src/audiotoolbox/viz.py
+++ src/audiotoolbox/viz.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from typing import Union
 
 COLOR_R = "#d65c5c"
 COLOR_L = "#5c5cd6"
@@ -33,7 +35,6 @@ class Visualization(object):
         ax : numpy.ndarray
             The array of axes objects for the subplots.
         """
-        import matplotlib.pyplot as plt
 
         assert self.sig.n_channels == 1, "Only single channel signals supported"
 
@@ -101,7 +102,6 @@ class Visualization(object):
             figure is created. (default is None)
 
         """
-        import matplotlib.pyplot as plt
 
         assert np.ndim(self.sig) <= 2, "Only 1 dimensional channel shapes allowed"
 
@@ -116,4 +116,74 @@ class Visualization(object):
             ax.plot(self.sig.time, self.sig)
         ax.set_xlabel("Time / s")
         ax.set_ylabel("Amplitude")
+        return fig, ax
+
+    def spectrum(
+        self,
+        single_sided: bool = True,
+        minx: float = 20.0,
+        maxx: float = 20000.0,
+        power: bool = False,
+        in_db: bool = True,
+        ax: Union[None, plt.Axes] = None,
+    ) -> tuple[plt.Figure, plt.Axes]:
+        """Plot the spectrum of the Signal using matplotlib.
+
+        This function computes and plots the amplitude spectrum of the signal.
+
+        Parameters
+        ----------
+        single_sided : bool
+            If True, only the positive frequencies are plotted.
+            (default is True)
+        minx : float
+            Minimum x-axis value in Hz. (default is 20.0).
+        maxx : float
+            Maximum x-axis value in Hz. (default is 20000.0). Values above
+            the Nyquist frequency are clamped to the Nyquist frequency.
+        power : bool
+            If True, the power spectrum is plotted instead of the amplitude
+            spectrum. (default is False)
+        in_db : bool
+            If True, the amplitude/power values are converted to dB scale.
+            (default is True)
+        ax : None, matplotlib.axis (optional)
+            The axis that should be used for plotting. If None, a new
+            figure is created. (default is None)
+
+        """
+        import matplotlib.pyplot as plt
+
+        nyquist = self.sig.fs / 2.0
+        if maxx > nyquist:
+            maxx = nyquist
+
+        if not ax:
+            fig, ax = plt.subplots(1, 1)
+        else:
+            fig = ax.figure
+
+        fsig = self.sig.to_freqdomain()
+        freq = fsig.freq
+        amplitude = np.abs(fsig)
+        if in_db:
+            amplitude = 20 * np.log10(amplitude + 1e-12)
+        elif power:
+            amplitude = amplitude**2
+
+        if single_sided:
+            half_n = len(freq) // 2
+            freq = freq[:half_n]
+            amplitude = amplitude[:half_n]
+            amplitude *= 2  # compensate for single sided spectrum
+
+        min_f = 20  # Hz
+        max_f = 20000  # Hz
+
+        ax.plot(freq, amplitude)
+        ax.set_xlabel("Frequency / Hz")
+        ax.set_ylabel("Amplitude")
+        if power:
+            ax.set_ylabel("Power")
+        ax.set_xscale("log")
         return fig, ax
